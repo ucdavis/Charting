e731a105d66ca851f98ae294cb819123f3957034
Scott Kirkland <srkirkland@gmail.com>
2013-01-14 15:40:18 -0800

    successful ajax call to generate chart

---
+++ Charting/Controllers/OrderHistoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.SessionState;
+using Charting.Models;
+using UCDArch.Web.ActionResults;
+
+namespace Charting.Controllers
+{
+    [SessionState(SessionStateBehavior.Disabled)]
+    public class OrderHistoryController : Controller
+    {
+        private static List<OrderHistory> _orders;
+        
+        public OrderHistoryController()
+        {
+            _orders = OrderHistory.GenerateHistory(2000).ToList();
+        }
+
+        public JsonNetResult OrdersByMonth()
+        {
+            var orderCountByMonth = from o in _orders
+                                    group o by o.DateCreated.Month
+                                    into months
+                                    orderby months.Key
+                                    select new { months.Key, Count = months.Count() };
+
+            return new JsonNetResult(orderCountByMonth.Select(x => new[] {x.Key, x.Count}));
+        }
+    }
+}
