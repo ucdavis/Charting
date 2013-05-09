1205fec0f602ecf75d9a8515490c9231b5cca999
Scott Kirkland <srkirkland@gmail.com>
2013-05-09 14:12:09 -0700

    bullet chart for professor score averages

---
+++ Charting/Controllers/D3Controller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UCDArch.Web.ActionResults;
+
+namespace Charting.Controllers
+{
+    public class D3Controller : Controller
+    {
+        public ActionResult Bullet()
+        {
+            return View();
+        }
+
+        public ActionResult BulletData()
+        {
+            var data = new List<BulletData>
+                {
+                    new BulletData
+                        {
+                            title = "MAT 127A",
+                            subtitle = "Scores against Department Avg",
+                            ranges = new[] {3, 3.5, 4.2},
+                            measures = new[] {3.67},
+                            markers = new[] {3.75}
+                        },
+                    new BulletData
+                        {
+                            title = "Overall Scores",
+                            subtitle = "Comparing all evals",
+                            ranges = new[] {3, 3.5, 4.25},
+                            measures = new[] {3.8},
+                            markers = new[] {4.12}
+                        }
+                };
+
+            return new JsonNetResult(data);
+        }
+    }
+
+    public class BulletData
+    {
+        public string title { get; set; }
+        public string subtitle { get; set; }
+        public double[] ranges { get; set; }
+        public double[] measures { get; set; }
+        public double[] markers { get; set; }
+    }
+}
